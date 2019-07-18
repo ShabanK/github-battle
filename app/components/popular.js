040d7af9dc458a542dc6c772856789b03099e38a
ShabanK <shabankhawar99@gmail.com>
2019-07-18 05:38:29 -0700

    i did it yatta

--- app/components/popular.js
+++ app/components/popular.js
@@ -3,10 +3,32 @@ import React from 'react';
 const lang = ["ALL", "Javascript", "CSS", "Java","Python","Ruby"]
 
 class Popular extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            selectedLanguage: "ALL"
+        }
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    updateLanguage(selectedLanguage){
+        this.setState({
+            selectedLanguage
+        })
+    }
+    
     render(){
         return(
             <ul className="flex-center">
-                {lang.map((loop)=>(<li key={loop}><button className="btn-clear nav-link">{loop}</button></li>))}
+                {lang.map((loop)=>(
+                    <li key={loop}>
+                        <button
+                            style={loop === this.state.selectedLanguage?{color: "red"}: null }
+                            className="btn-clear nav-link"
+                            onClick={()=>this.updateLanguage(loop)}
+                            >
+                            {loop}
+                        </button>
+                    </li>))}
             </ul>
         )
     }
